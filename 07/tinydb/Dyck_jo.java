cc19f521b655b57dcf59624896a4852ae417a82b
jmerkle <johannesmerkle@gmx.de>
2012-06-17 19:39:34 +0200

    dyck_jo works (somehow)

--- 07/tinydb/Dyck_jo.java
+++ 07/tinydb/Dyck_jo.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 
 public class Dyck_jo {
 
@@ -6,36 +8,48 @@ public class Dyck_jo {
 //		test(4, 1);
 //		test(4, 2);
 		test(8, 56);
+		//System.out.println(Dyck_jo.q(0, 0, 8));
 	}
 	
 	private static void test(int n, int i) {
-		System.out.println("n: "+n+", i: "+i+" -> "+unrankTree(n, i));
+		System.out.print("n: "+n+", i: "+i+" -> ");
+		LinkedList<Integer> t = unrankTree(n,i);
+		for (int e : t){
+			System.out.print(e+" ");
+		}
+		System.out.println();
 	}
 	
-	public static String unrankTree(double n, double i){
+	public static LinkedList<Integer> unrankTree(double n, double i){
 		int open = 1;
 		int close = 0;
 		int pos = 1;
-		String encoding = "1";
-		while (encoding.length()<n){
-			pos++;
-			System.out.println("open: "+open+" close: "+close+" pos: "+pos);
-			double k = q(open+close, open-close, n);
-			System.out.println("q: "+k);
+		LinkedList<Integer> encoding = new LinkedList<Integer>();
+		encoding.add(1);
+		//n--;n--;
+		double n_t = n-1;
+		while (encoding.size()<n){
+			
+			//System.out.println("open: "+open+" close: "+close+" pos: "+pos);
+			double k = q(open+close, open-close, n_t);
+			System.out.print("q: "+k);
 			if (k<=i){//rank is bigger than q: step down, rank = rank-q
+				System.out.println(", down!");
 				i=i-k;
 				close++;
 			} else {//rank is smaller than q: step up
-				encoding = encoding + Integer.toString(pos);
+				System.out.println(", up!");
+				encoding.add(pos+1);
 				open++;
 			}
-			
+			pos++;
 		}
 		return encoding;
 	}
 	
 	//number of different paths from (i,j) to (2n,0)
 	public static double q(double i, double j, double n){
+		System.out.println("called q with i="+i+", j="+j+", n="+n);
 		return Dyck_jo.p(2*n-i, j);
 	}
 	
