4bc10b93ddbf74f6125a8602b4754ee683108651
vogler <ralf.vogler@gmail.com>
2012-07-13 19:48:20 +0200

    fixed plots

--- 11/ex4.m
+++ 11/ex4.m
@@ -1,5 +1,4 @@
-close all;
-clear all;
+close all; clear all; clc;
 
 m = 100;
 n = 10;
@@ -12,52 +11,53 @@ Y_b = zeros(1,N);
 
 for k = 1:1000
     if (k<=N-n) 
+        % Yao
         %p = nchoosek(N-n,k)/nchoosek(N,k); %not exact
-        %Yao
         X = 0:k-1;
         p = prod((N-n-X)./(N-X));
         Y(k) = (1-p)*m;
-        %Waters
+        % Waters
         Y_w(k) = m*(1-(1-k/N)^n);
     else
         Y(k) = m;
         Y_w(k) = m;
     end
     
-    %Bernstein
+    % Bernstein
     if (k<m/2)
-        Y_b(k)=k;
+        Y_b(k) = k;
     elseif (k<2*m)
-        Y_b(k)=(k+m)/3;
+        Y_b(k) = (k+m)/3;
     else
         Y_b(k) = m;
     end
 end
 
-title('combined plot');
-ylabel('accessed pages');
-xlabel('tuples');
+
 plot(Y, 'Color', 'Red');
 hold all;
 plot(Y_w, 'Color', 'Blue');
 hold all;
 plot(Y_b, 'Color', 'Green');
+legend('Yao', 'Waters', 'Bernstein');
+title('Combined plot');
+ylabel('accessed pages');
+xlabel('tuples');
 
 figure;
+plot(Y, 'Color', 'Red');
 title('Yao');
 ylabel('accessed pages');
 xlabel('tuples');
-plot(Y, 'Color', 'Red');
 
 figure;
+plot(Y_w, 'Color', 'Blue');
 title('Waters');
 ylabel('accessed pages');
 xlabel('tuples');
-plot(Y_w, 'Color', 'Blue');
 
 figure;
+plot(Y_b, 'Color', 'Green');
 title('Bernstein');
 ylabel('accessed pages');
 xlabel('tuples');
-plot(Y_b, 'Color', 'Green');
-
